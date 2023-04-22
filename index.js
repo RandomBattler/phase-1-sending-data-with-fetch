ed29e5e48e7b39bb1eb6dbb8994962c9bd6e4e50
Steve Romero <romero.steve.a@gmail.com>
2023-04-22 12:53:59 -0700

    Unauthorized access error

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Add your code here
+function submitData(name_, email_){
+    const configObject = {
+        method: "POST",
+        headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+        },
+        body: JSON.stringify({
+        name: name_,
+        email: email_,
+        })
+    }
+    return fetch("http://localhost:3000/users", configObject)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (object) {
+      console.log(object.id);
+    })
+    .catch(function (error) {
+      alert("Bad things! Ragnarők!");
+      console.log(error.message);
+    });
+}
